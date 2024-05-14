382f89acfd542323b5348efda44d6616e62265af
tengshuang <tengshuang@cjbdi.com>
2024-05-14 17:12:05 +0800

    增加1.2.2版本新配置

--- scripts/dispatcher-worker.sh
+++ scripts/dispatcher-worker.sh
@@ -19,12 +19,12 @@ BASE_PATH=$(
 INIT_PATH=$(dirname "${BASE_PATH}")
 echo "INIT_PATH: ${INIT_PATH}"
 
-cp /opt/datasophon/DDP/packages/datasophon-worker.tar.gz /opt/datasophon/
-checkworkmd5=$(md5sum /opt/datasophon/DDP/packages/datasophon-worker.tar.gz | awk '{print $1}')
+cp /data/apps/DDP/packages/datasophon-worker.tar.gz /data/apps/
+checkworkmd5=$(md5sum /data/apps/DDP/packages/datasophon-worker.tar.gz | awk '{print $1}')
 if [ "$checkworkmd5" = "$workmd5" ]; then
   echo "md5效验通过"
 else
   echo "md5效验不通过"
   exit 1
 fi
-tar -zxvf /opt/datasophon/datasophon-worker.tar.gz -C /opt/datasophon/
+tar -zxvf /data/apps/datasophon-worker.tar.gz -C /data/apps/
